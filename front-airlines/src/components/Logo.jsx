e65ad1d2090491c5c950d066a732606dd1901e40
Nestor-Sanchez-Sanchez <117288771+Nestor-Sanchez-Sanchez@users.noreply.github.com>
2022-11-29 09:54:05 +0100

    develop - First Commit

---
+++ front-airlines/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import images from '../images.png';
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={images} />
+        </div>
+    );
+}
+
+export default Logo;
