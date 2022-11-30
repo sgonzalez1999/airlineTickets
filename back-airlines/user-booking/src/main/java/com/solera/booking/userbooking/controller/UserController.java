b3c53b6a822cb24a39e5e1f7b6cc97b794cabb74
Nestor-Sanchez-Sanchez <117288771+Nestor-Sanchez-Sanchez@users.noreply.github.com>
2022-11-30 14:30:13 +0100

    Nestor - useNavigate Login

--- back-airlines/user-booking/src/main/java/com/solera/booking/userbooking/controller/UserController.java
+++ back-airlines/user-booking/src/main/java/com/solera/booking/userbooking/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.solera.booking.userbooking.controller;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	
-	@GetMapping()
-	public String sayHi() {
-		return "Hi User";
+	String user = "pepe";
+	
+	
+	@GetMapping("/{name}")
+	public ResponseEntity<String> sayHi(@PathVariable String name) {
+		System.out.println("entrada " + name);
+		if(user.equals(name)) {
+			return new ResponseEntity<>("Hi User", HttpStatus.OK);
+		}
+		return new ResponseEntity<>("OH NOOOOOO", HttpStatus.NOT_FOUND);
+		
 	}
 }
